982c2d07656609decd3e243d7af49d3cbf989fba
Michael Muehlberger <michael.muehlberger@edu.fh-joanneum.at>
2023-06-01 11:25:08 +0200

    Created logger object in Main

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,13 +1,18 @@
 package at.fhj.msd;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class Main {
+
+    //Creating a logger
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
 
         double number1 = 5.0;
-        double number2 = 0.0;
+        double number2 = 2.0;
 
         //+
         System.out.printf("Operation: %f + %f = ", number1, number2);
@@ -24,10 +29,18 @@ public class Main {
         System.out.println(calculator.multiply(number1,number2));
         System.out.println();
 
-        //+
+        ///
         System.out.printf("Operation: %f / %f = ", number1, number2);
         System.out.println(calculator.divide(number1,number2));
         System.out.println();
+
+        //!
+        System.out.printf("Operation: !%d = ", (int) number1);
+        System.out.println(calculator.faculty((int) number1));
+        System.out.println();
+
+        logger.info("It is an info logger.");
+        logger.error("It is an error logger.");
 
     }
 
